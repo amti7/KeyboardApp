44779b09595d4197bfaceb6153c7093a0e45457e
Kamil <kamil@MacBook-Air-Kamil-2.local>
2021-12-17 14:01:47 +0100

    Adding Settings Button

--- KeyboardApp/ViewController.swift
+++ KeyboardApp/ViewController.swift
@@ -12,10 +12,12 @@ final class ViewController: UIViewController {
     
     private let mainTextField = UITextField()
     private let keyboardEnabledLabel = UILabel()
+    private let settingsButton = UIButton()
+    private let buttonImage = UIImageView()
     
     private var willEnterForegroundNotification: NSObjectProtocol?
     
-    let keyboardState = KeyboardEnabledState(bundleId: C.newKeyboardKitBundleID)
+    var keyboardState = KeyboardEnabledState(bundleId: C.newKeyboardKitBundleID)
     
     var appearance: UIKeyboardAppearance = .default
 
@@ -28,16 +30,20 @@ final class ViewController: UIViewController {
         addSubviews()
         setupTextField()
         setupKeyboardEnabledLabel()
+        setupSettingsButton()
+        
+        
     }
     
     private func setupObservers() {
         willEnterForegroundNotification = NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main) { [weak self] notification in
+            self?.keyboardState = KeyboardEnabledState(bundleId: C.newKeyboardKitBundleID)
             self?.checkIfKeyboardEnabled()
         }
     }
     
     private func addSubviews() {
-        [mainTextField, keyboardEnabledLabel].forEach {
+        [mainTextField, keyboardEnabledLabel, settingsButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
@@ -54,14 +60,38 @@ final class ViewController: UIViewController {
     }
     
     private func checkIfKeyboardEnabled() {
-//        print("(*) enabled:", keyboardState.isKeyboardEnabled)
         if keyboardState.isKeyboardEnabled {
-            keyboardEnabledLabel.text = "Keyboard is Enabled V"
+            keyboardEnabledLabel.text = C.keyboardEnabledString
             keyboardEnabledLabel.textColor = .systemGreen
         } else {
-            keyboardEnabledLabel.text = "Keyboard is Disabled X"
+            keyboardEnabledLabel.text = C.keyboardDisabledString
             keyboardEnabledLabel.textColor = .systemRed
         }
+    }
+    
+    private func setupSettingsButton() {
+        settingsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        settingsButton.topAnchor.constraint(equalTo: keyboardEnabledLabel.bottomAnchor, constant: C.sideOffset).isActive = true
+        settingsButton.heightAnchor.constraint(equalToConstant: C.heightTextfieldOffset).isActive = true
+        settingsButton.widthAnchor.constraint(equalToConstant: C.widthTextfieldOffet).isActive = true
+        
+        settingsButton.backgroundColor = .systemGray2
+        settingsButton.layer.cornerRadius = C.cornerRadiusValue
+        settingsButton.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
+        
+        settingsButton.addSubview(buttonImage)
+        setupButtonImage()
+        
+    }
+    
+    private func setupButtonImage() {
+        buttonImage.translatesAutoresizingMaskIntoConstraints = false
+        buttonImage.centerXAnchor.constraint(equalTo: settingsButton.centerXAnchor).isActive = true
+        buttonImage.centerYAnchor.constraint(equalTo: settingsButton.centerYAnchor).isActive = true
+        buttonImage.heightAnchor.constraint(equalToConstant: C.buttonImageSize).isActive = true
+        buttonImage.widthAnchor.constraint(equalToConstant: C.buttonImageSize).isActive = true
+        
+        buttonImage.image = UIImage(named: C.settingsIconString)
     }
     
     private func setupTextField() {
@@ -74,6 +104,16 @@ final class ViewController: UIViewController {
         mainTextField.layer.cornerRadius = C.cornerRadiusValue
         mainTextField.textAlignment = .center
         mainTextField.textColor = .black
+        
+        
+    }
+    
+    @objc private func openSettings() {
+        guard let url = URL(string: C.keyboardSetttingsPrefs) else { return}
+        
+        DispatchQueue.main.async {
+            UIApplication.shared.open(url)
+        }
     }
 }
 
@@ -86,13 +126,16 @@ extension ViewController {
         static let borderWidthValue: CGFloat = 1.0
         static let sideOffset: CGFloat = 16
         
+        static let buttonImageSize: CGFloat = 30
+        
         static let keyboardLabelHeight: CGFloat = 50
         static let keyboardLabelTopOffset: CGFloat = 32
         static let newKeyboardKitBundleID: String = "com.dot.KeyboardApp.SomeKeyboard"
-        
         static let lightModeString: String = "Light Mode"
         static let darkModeString: String = "Dark Mode"
-        
-        
+        static let keyboardEnabledString: String = "Keyboard is Enabled V"
+        static let keyboardDisabledString: String = "Keyboard is Disabled X"
+        static let keyboardSetttingsPrefs: String = "App-prefs:General&path=Keyboard"
+        static let settingsIconString: String = "settingsIcon"
     }
 }
